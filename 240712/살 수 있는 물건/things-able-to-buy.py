234f1d5ceb8d686de24e11fc5908746096d172b4
haein0102 <happyhi0102@gmail.com>
2024-07-12 14:55:54 +0900

    Title: 살 수 있는 물건, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240712/살 수 있는 물건/things-able-to-buy.py
+++ 240712/살 수 있는 물건/things-able-to-buy.py
@@ -6,3 +6,10 @@ elif n<1000:
     print("no")
 else:
     print("mask")
+
+#만약 코드가 if n >= 3000:
+#	print("book");
+#elif n >= 1000:
+#	print("mask");
+#else:
+#	print("no"); 이렇게 되면 n이 4000 이런식으로 되면 if문이랑 elif문 동시에 성립해서 book이랑 mask가 동시에 나오는 건 아닌지?
