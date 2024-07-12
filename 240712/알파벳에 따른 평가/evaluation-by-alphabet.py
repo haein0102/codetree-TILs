916b1fee0df433a19dcd5bdd3592721269be0f69
haein0102 <happyhi0102@gmail.com>
2024-07-12 15:35:11 +0900

    Title: 알파벳에 따른 평가, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240712/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+L=input()
+
+if L=="S":
+    print("Superior")
+elif L=="B":
+    print("Good")
+elif L=="A":
+    print("Excellent")
+elif L=="C":
+    print("Usually")
+elif L=="D":
+    print("Effort")
+else:
+    print("Failure")
