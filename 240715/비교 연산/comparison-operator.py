b3632021f0b2ddc9890bff97af040cd70e0e7216
haein0102 <happyhi0102@gmail.com>
2024-07-15 16:52:28 +0900

    Title: 비교 연산, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240715/비교 연산/comparison-operator.py
@@ -0,0 +1,17 @@
+a,b=input().split()
+a=int(a)
+b=int(b)
+
+result1 = 1 if a >=b else 0
+result2 = 1 if a > b else 0
+result3 = 1 if a <= b else 0
+result4 = 1 if a <b else 0
+result5 = 1 if a==b else 0
+result6 = 1 if a!=b else 0
+# 결과 출력
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+print(result5)
+print(result6)
