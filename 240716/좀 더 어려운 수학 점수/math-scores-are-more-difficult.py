59f425dfbfe9c8bec2f892b7ce722a52dc3494a2
haein0102 <happyhi0102@gmail.com>
2024-07-16 21:07:13 +0900

    Title: 좀 더 어려운 수학 점수, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240716/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -0,0 +1,15 @@
+a_math, a_english=input().split()
+b_math, b_english=input().split()
+a_math=int(a_math)
+a_english=int(a_english)
+b_math=int(b_math)
+b_english=int(b_english)
+
+if a_math>b_math:
+    print("A")
+elif a_math<b_math:
+    print("B")
+elif a_math==b_math and a_english>b_english:
+    print("A")
+else:
+    print("B")
