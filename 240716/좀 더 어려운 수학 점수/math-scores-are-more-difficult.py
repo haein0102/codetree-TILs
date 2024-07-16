95d75a56ccc3b000bb6136592a840829162d1106
haein0102 <happyhi0102@gmail.com>
2024-07-16 21:07:47 +0900

    Title: 좀 더 어려운 수학 점수, Time: 189ms, Memory: 24MB, Status: Passed - CodeTree

--- 240716/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
+++ 240716/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -13,3 +13,8 @@ elif a_math==b_math and a_english>b_english:
     print("A")
 else:
     print("B")
+
+#if a_math > b_math or (a_math == b_math and a_eng > b_eng):
+#    print("A")
+#else:
+#    print("B")
