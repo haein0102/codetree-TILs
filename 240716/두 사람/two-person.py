7aa511dd6eaa3e9d9c26454ca210a078a816c08e
haein0102 <happyhi0102@gmail.com>
2024-07-16 21:28:21 +0900

    Title: 두 사람, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240716/두 사람/two-person.py
+++ 240716/두 사람/two-person.py
@@ -1,10 +1,24 @@
-A_age, A_sex= input().split()
-B_age, B_sex=input().split()
+#A_age, A_sex= input().split()
+#B_age, B_sex=input().split()
 
-A_age=int(A_age)
-B_age=int(B_age)
+#A_age=int(A_age)
+#B_age=int(B_age)
 
-if (A_sex == 'M' or B_sex == 'M') and (A_age>=19 or B_age >=19):
+#if (A_sex == 'M' or B_sex == 'M') and (A_age>=19 or B_age >=19):
+#    print(1)
+#else:
+#    print(0)
+
+# 첫 번째 사람의 정보 입력받기
+age1, gender1 = input().split()
+age1 = int(age1)
+
+# 두 번째 사람의 정보 입력받기
+age2, gender2 = input().split()
+age2 = int(age2)
+
+# 조건 판단 및 출력
+if (age1 >= 19 and gender1 == 'M') or (age2 >= 19 and gender2 == 'M'):
     print(1)
 else:
     print(0)
