647b60a7f29fbc684b1bd80b47525464043a6904
haein0102 <happyhi0102@gmail.com>
2024-07-17 15:15:01 +0900

    Title: 홀수만 출력, Time: 230ms, Memory: 24MB, Status: Passed - CodeTree

--- 240717/홀수만 출력/output-only-odd.py
+++ 240717/홀수만 출력/output-only-odd.py
@@ -2,9 +2,5 @@ a,b=input().split()
 a=int(a)
 b=int(b)
 
-if a%2==0 and b%2==0:
-    for i in range(a+,b+2,2):
-        print(i, end=" ")
-else:
-    for i in range(a,b+1,2):
+for i in range(a,b+1,2):
         print(i, end=" ")
