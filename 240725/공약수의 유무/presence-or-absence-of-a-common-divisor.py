b5a5d925cdb2bbca19d67d7b165c2a30ff22379f
haein0102 <happyhi0102@gmail.com>
2024-07-25 15:10:24 +0900

    Title: 공약수의 유무, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240725/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -0,0 +1,12 @@
+a,b=input().split()
+a=int(a)
+b=int(b)
+satisfied=False
+
+for i in range(a,b+1):
+    if 1920%i==0 and 2880%i==0:
+        satisfied=True
+if satisfied==True:
+    print(1)
+else:
+    print(0)
